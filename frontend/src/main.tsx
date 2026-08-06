4721d1dab8725a5ddbf4afa1ebf3a6aa01abd53c
Wodoame <bmwodoame@gmail.com>
2026-08-06 13:32:06 +0000

    feat(frontend): wire up Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Rankings from "./Rankings.tsx";
 import FAQs from "./FAQs.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -30,6 +31,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 );
